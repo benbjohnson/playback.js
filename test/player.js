fce064a9c69cfc0656f90563aeeaa9f942414de7
Ben Johnson <benbjohnson@yahoo.com>
2013-11-04 08:07:31 -0700

    Add D3.js test.

--- test/player.js
+++ test/player.js
@@ -1,32 +1,68 @@
 describe('Player', function(){
 
-  var Player = require('d3.player/lib/player')
-    , assert = require('assert')
-    , equals = require('equals');
+  var Player   = require('d3.player/lib/player')
+    , assert   = require('assert')
+    , equals   = require('equals')
+    , nextTick = require('next-tick');
 
-  var player = null;
+  var player = null, svg = null, g = null;
   beforeEach(function() {
     player = new Player();
+    $("#scratch").empty();
+    svg = d3.select("#scratch").append("svg");
+    g = svg.append("g");
   });
 
-  describe('#refresh()', function(){
-    it('should set the refresh function and call it', function(){
-      var spy = sinon.spy();
-      player.refresh(spy);
-      assert(spy.called)
+  describe('Player()', function(){
+    it('should set the selector', function(){
+      player = new Player("div");
+      assert(player.selector() == "div");
+    });
+  });
+
+  describe('#selector()', function(){
+    it('should set and get the selector', function(){
+      assert(player.selector("rect") == player);
+      assert(player.selector() == "rect");
+    });
+  });
+
+  describe('#play()', function(){
+    beforeEach(function() {
+      player.pause();
     });
 
-    it('should execute refresh function when called', function(){
-      var spy = sinon.spy();
-      player.refresh(spy);
-      spy.reset()
-      assert(!spy.called)
-      player.refresh();
-      assert(spy.called)
+    it('should update the player to playing', function(){
+      player.play();
+      assert(player.playing());
+    });
+  });
+
+  describe('#pause()', function(){
+    beforeEach(function() {
+      player.play();
     });
 
-    it('should do nothing when refresh is not set', function(){
-      player.refresh();
+    it('should update the player to a paused state', function(){
+      player.pause();
+      assert(!player.playing());
+    });
+
+    it('should remove transitions on selection', function(done){
+      var data = [{r:1}, {r:2}];
+      player.selector("circle");
+      g.selectAll("circle").data(data)
+        .call(function() {
+          this.enter().append("circle").attr("r", 0);
+          this.transition().attr("r", function(d) { return d.r; });
+        });
+      player.pause();
+      setTimeout(function() {
+        g.selectAll("circle").each(function(e) {
+          assert(this.__transition__ === undefined);
+        });
+        done();
+      }, 50);
     });
   });
 });
